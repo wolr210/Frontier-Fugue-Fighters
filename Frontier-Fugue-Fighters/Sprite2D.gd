fde6d8c593432cb2940381450216c134b76cc0d1
Jay Rana <jay.rana1@gmail.com>
2023-04-08 13:19:44 -0400

    Added game files

---
+++ Frontier-Fugue-Fighters/Sprite2D.gd
@@ -0,0 +1,43 @@
+extends Sprite2D
+
+var startFrameCount = 0
+var player1InputFrame = 0
+var player1Input = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	DisplayServer.window_set_size(Vector2(800,800))#(DisplayServer.screen_get_size())
+	DisplayServer.window_set_position(Vector2(0,0))
+	Engine.set_max_fps(60)
+	startFrameCount = Engine.get_frames_drawn()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	var currentFrameCount = Engine.get_frames_drawn()
+	var framesTillNextBeat = (currentFrameCount - startFrameCount) % GlobalVars.FRAMES_PER_BEAT
+	if framesTillNextBeat <= 1 or framesTillNextBeat >= 29:
+		position = Vector2(100,100)
+	else:
+		position = Vector2(600,600)
+	if player1Input:
+		player1Input = false
+		var player1AlignedInput = (player1InputFrame - startFrameCount) % GlobalVars.FRAMES_PER_BEAT
+		if player1AlignedInput <= GlobalVars.TIMING_PERFECT / 2:
+			print("PERFECT")
+		elif player1AlignedInput <= (GlobalVars.TIMING_PERFECT / 2) + GlobalVars.TIMING_LATE_PERFECT:
+			print("LATE PERFECT")
+		elif player1AlignedInput <= GlobalVars.FRAMES_PER_BEAT / 2:
+			print("LATE")
+		elif player1AlignedInput >= GlobalVars.FRAMES_PER_BEAT - (GlobalVars.TIMING_PERFECT / 2) - 1:
+			print("PERFECT")
+		elif player1AlignedInput >= GlobalVars.FRAMES_PER_BEAT - (GlobalVars.TIMING_PERFECT / 2) - GlobalVars.TIMING_EARLY_PERFECT - 1:
+			print("EARLY PERFECT")
+		elif player1AlignedInput >= (GlobalVars.FRAMES_PER_BEAT / 2) + 1:
+			print("EARLY")
+	
+func _input(_ev):
+	if Input.is_key_pressed(KEY_K):
+		player1InputFrame = Engine.get_frames_drawn()
+		player1Input = true
+	elif Input.is_key_pressed(KEY_ESCAPE):
+		get_tree().quit()
---
+++ Frontier-Fugue-Fighters/data/constants.gd
@@ -0,0 +1,26 @@
+extends Node
+
+# BPM and other related variables
+const BPM = 120
+const FRAMES_PER_BEAT = 3600 / BPM
+
+# TIMING and other related variables
+const TIMING_EARLY_PERFECT = 3 # number of frames that you can input an early perfect
+const TIMING_PERFECT = 4 # number of frames that you can input a perfect; in the code, it is divided by two and checked on each side of the beat
+const TIMING_LATE_PERFECT = 3 # number of frames that you can input a late perfect
+
+# GLOBAL ATTACK related variables
+const MOVE_TYPE_NONE   = 0
+const MOVE_TYPE_ATTACK = 1
+const MOVE_TYPE_BLOCK  = 2
+const MOVE_TYPE_GRAB   = 3
+
+const ATTACK_POWER_MAX  = 20 # attack damage when on beat
+const BLOCK_PERCENT_MAX = 1.0 # percent of damage blocked when on beat (0.0 = no block, 1.0 = full block)
+const GRAB_DURATION_MAX = 4 # number of beats that the grab is held for
+
+const MOVE_SCALE_EARLY         = 0.25 # when attacker is early, scale move power/percent/duration by this much
+const MOVE_SCALE_EARLY_PERFECT = 0.5 # when attacker is early perfect, scale move power/percent/duration by this much
+const MOVE_SCALE_PERFECT       = 1 # when attacker is perfect, scale move power/percent/duration by this much
+const MOVE_SCALE_LATE_PERFECT  = 0.5 # when attacker is late perfect, scale move power/percent/duration by this much
+const MOVE_SCALE_LATE          = 0.25 # when attacker is late, scale move power/percent/duration by this much
